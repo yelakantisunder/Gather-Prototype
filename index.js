a95f79d2913dbcb18b0580a11406bb75e78b450f
yelakantisunder <Ysunder7@gmail.com>
2021-12-09 12:28:37 +0530

    nearby function successful

--- index.js
+++ index.js
@@ -27,6 +27,14 @@
 // ctx.fillRect(w,h,20,20);
 // }
 
+//Global varibales
+var obj = {};
+var id;
+let x = 0;
+let y = 0;
+let w = 500;
+let h = 500;
+
 //Function to move the avatar
 function move(e) {
   if (e.keyCode == 39) {
@@ -41,80 +49,54 @@ function move(e) {
   if (e.keyCode == 40) {
     y += 5;
   }
-  c.width = c.width;
-  ctx.drawImage(img2, x, y);
-  ctx.drawImage(img1, w, h);
+  loadCanvas(id);
+
+  //Checking  avatars in proximity
+  nearby();
+}
+
+//Function to check nearby avatars
+function nearby() {
+  console.log(obj);
+  var distance = Math.floor(
+    Math.sqrt(
+      Math.pow(obj["B"][0] - obj["A"][0], 2) +
+        Math.pow(obj["B"][1] - obj["A"][1], 2)
+    )
+  );
+
+  if (distance < 250) {
+    console.log("Avatar nearby turn on mic and video");
+  }
 }
 
 //keydown event calls move fn whenever arrow keys are pressed.
 document.onkeydown = move;
 
-function createAvatar() {
-  var cardbody = `<div class="card mt-5" style="width: 28rem;  margin:auto;">
-  <div class="card-body">
-    <h5 class="card-title" style="text-align:center;">Gather Town</h5>
-    <p class="card-text" style="text-align:center;">Select your avatar!!</p>
-    <div class="container">
-        <div class="row">
-            <div class="col">
-            <img src="1.png" style="width:50px; height:50px;" onclick="loadCanvas(1)"/>
-            </div>
-            <div class="col">
-            <img src="2.png" style="width:50px; height:50px;" onclick="loadCanvas(2)"/>
-            </div>
-            <div class="col">
-            <img src="3.png" style="width:50px; height:50px;" onclick="loadCanvas(3)"/>
-            </div>
-            <div class="col">
-            <img src="4.png" style="width:50px; height:50px;" onclick="loadCanvas(4)"/>
-            </div>
-        </div>
-        <br/>
-
-        <div class="row">
-            <div class="col">
-            <img src="5.png" style="width:50px; height:50px;" onclick="loadCanvas(5)"/>
-            </div>
-            <div class="col">
-            <img src="6.png" style="width:50px; height:50px;" onclick="loadCanvas(6)"/>
-            </div>
-            <div class="col">
-            <img src="7.png" style="width:50px; height:50px;" onclick="loadCanvas(7)"/>
-            </div>
-            <div class="col">
-            <img src="8.png" style="width:50px; height:50px;" onclick="loadCanvas(8)"/>
-            </div>
-        </div>
-    </div>
-  </div>
-</div>`;
-  document.getElementById("root").innerHTML = cardbody;
-}
-
 //function to display the canvas
-function loadCanvas(id) {
-  console.log(id);
-  var player1 = id + ".png";
+function loadCanvas(pic) {
+  id = pic;
+
   var canvas = `<canvas id="myCanvas" width="1000" height="700" style="border:1px solid #000000; background-image: url('office.png'); background-size: contain;"></canvas>`;
   document.getElementById("root").innerHTML = canvas;
   var c = document.getElementById("myCanvas");
   var ctx = c.getContext("2d");
+
   //required when avatar images are added
   var img1 = new Image();
   var img2 = new Image();
-  img1.src = player1;
-  console.log(player1);
-  //img2.src = "8.png";
-  let x = 0;
-  let y = 0;
 
-  let w = Math.floor(Math.random() * 1000);
-  let h = Math.floor(Math.random() * 700);
+  var player1 = pic + ".png";
+  img1.src = player1;
+  img2.src = "8.png";
+
   //Creating new images
   img1.onload = () => {
     ctx.drawImage(img1, w, h);
+    obj["A"] = [w, h];
   };
-  //   img2.onload = () => {
-  //     ctx.drawImage(img2, x, y);
-  //   };
+  img2.onload = () => {
+    ctx.drawImage(img2, x, y);
+    obj["B"] = [x, y];
+  };
 }
